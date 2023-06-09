9ad4667c4f1ad73f72d3c7a184ebaba3970d536d
Rodrigue <rodkamd@gmail.com>
2023-06-09 16:19:25 +0200

    Add project files.

---
+++ FileDonwloadManagerWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FileDonwloadManagerWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
